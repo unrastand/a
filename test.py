e9e62bde4965beb01d5d0b37ba08b7b175e234e5
Your Name <kolawoleisrael2500@gmail.com>
2026-04-11 18:03:34 +0100

    session 3

--- test.py
+++ test.py
@@ -1,12 +1,91 @@
-# variables
-a = 5
-b = 9.0
-c = "Hello World"
-d = False 
+# created a variable age
+age = 56
+# created a variable ratio
+ratio = 7.8
+# created a variable name
+name = "James"
+# created a variable is_young
+is_young = False
 
-g = "8"
+# pascal case
+PascalCase = "This is Pascal Case"
+HighWay = 8
+# camel case
+camelCase = "This is Camel Case"
+highWay = 8
+# snake casesnake_case = "This is Snake Case"
+snake_case = "This is Snake Case"
 
-#arithmetic operators
+high_way = 8
+#toggle case
+toggleCase = "This is Toggle Case"
+hIGHwAY = 8
 
-# +   - * / %
-print(4 % 11)
+# print("welcome to AI and ML")
+# users_input = input("Enter your data ")
+# print("you inputted: " + users_input)
+
+
+# arithmetic operators
+# age = 8+ 10
+# print(f"the sum of 8 and 10 {age}")
+# 
+# range = 90-6
+# print(f"the substraction between 90 and 6 {range}")
+# 
+# barn = 7*7
+# print(f"the multiplication of 7 and 7 {barn}")
+# 
+# divide = 89/56
+# print(f"the division of 89 and 56 {divide}")
+# 
+# moduli = 25 % 5
+# print(f"the moduli of 25 and 5 {moduli}")
+# 
+# raise_to_power = 5 ** 2
+# print(f"the raise to power of 5 and 2 {raise_to_power}")
+
+
+# comparism operators
+
+#equal to operator, this checks if both are the same
+#print(5==5)
+
+#not equal to operators, so it checks if both are not the same
+# print(5 != 5)
+
+# greater than operator, it checks if the left hand side is greater than the right hand side
+# print(6 > 3)
+
+# less than operator, it checks if the left hand side is less than the right hand side
+# print(3 < 1)
+
+# greater than or equal to operator, it checks if the left hand side is greater 
+# than or equal to the right hand side
+# print(7 >= 7)
+
+# less than or equal to operator, it checks if the left hand side is less than 
+# or equal to the right hand side
+# print(8 <= 7)
+
+# assignment operators
+
+# assignment operator, it assigns a value to a variable
+house_number = 8
+
+# addition assignment operator, it adds the right hand side to the left hand side 
+# and assigns the result to the left hand side
+# house_number += 6
+# print(house_number)
+
+# house_number -= 2
+# print(house_number)
+
+# house_number *= 2
+# print(house_number)
+
+# syntax error
+
+main = 88
+print(main)
+print("this is the sum",  main)
